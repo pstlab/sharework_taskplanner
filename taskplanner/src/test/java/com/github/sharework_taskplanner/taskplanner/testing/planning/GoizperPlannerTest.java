3ac28817634372ecca55a4e1ad3158a0be9efb05
Alessandro Umbrico <umbrico.alessandro@gmail.com>
2021-09-17 21:24:39 +0200

    minor changes

---
+++ taskplanner/src/test/java/com/github/sharework_taskplanner/taskplanner/testing/planning/GoizperPlannerTest.java
@@ -0,0 +1,83 @@
+package com.github.sharework_taskplanner.taskplanner.testing.planning;
+
+import com.github.sharework_taskplanner.taskplanner.heuristics.flaws.ShareworkFlawSelectionHeuristics;
+import it.cnr.istc.pst.platinum.ai.deliberative.Planner;
+import it.cnr.istc.pst.platinum.ai.deliberative.PlannerBuilder;
+import it.cnr.istc.pst.platinum.ai.framework.domain.PlanDataBaseBuilder;
+import it.cnr.istc.pst.platinum.ai.framework.domain.component.PlanDataBase;
+import it.cnr.istc.pst.platinum.ai.framework.microkernel.annotation.cfg.FrameworkLoggerConfiguration;
+import it.cnr.istc.pst.platinum.ai.framework.microkernel.annotation.cfg.deliberative.FlawSelectionHeuristicsConfiguration;
+import it.cnr.istc.pst.platinum.ai.framework.microkernel.annotation.cfg.deliberative.PlannerSolverConfiguration;
+import it.cnr.istc.pst.platinum.ai.framework.microkernel.annotation.cfg.deliberative.SearchStrategyConfiguration;
+import it.cnr.istc.pst.platinum.ai.framework.microkernel.lang.ex.NoSolutionFoundException;
+import it.cnr.istc.pst.platinum.ai.framework.microkernel.lang.ex.ProblemInitializationException;
+import it.cnr.istc.pst.platinum.ai.framework.microkernel.lang.plan.SolutionPlan;
+import it.cnr.istc.pst.platinum.ai.framework.utils.log.FrameworkLoggingLevel;
+
+
+/**
+ *
+ */
+@PlannerSolverConfiguration(
+		timeout = 300000
+)
+@FlawSelectionHeuristicsConfiguration(
+		heuristics = ShareworkFlawSelectionHeuristics.class
+)
+@SearchStrategyConfiguration
+@FrameworkLoggerConfiguration(		
+		level = FrameworkLoggingLevel.OFF
+)
+public class GoizperPlannerTest extends Planner
+{
+	protected GoizperPlannerTest() {
+		super();
+	}
+	
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{ 
+		try 
+		{
+			// generate DDL and PDL
+			String[] model = new String[] {
+					"domains/sharework/goizper/hrc_goizper_v1.0.ddl",
+					"domains/sharework/goizper/hrc_goizper.pdl"
+			};
+			
+			
+			// build the plan database
+			PlanDataBase pdb = PlanDataBaseBuilder.createAndSet(
+					model[0], model[1]);
+			
+			// set a planning instance of the plan database
+			Planner planner = PlannerBuilder.createAndSet(
+					GoizperPlannerTest.class, pdb);
+
+			// start planning
+			SolutionPlan plan = planner.plan();
+			// solution found
+			System.out.println("----------------------------------\n"
+					+ "Solution found after " + plan.getSolvingTime() + " msecs\n"
+					+ "Solution plan:\n"
+					+ "" + plan + "\n"
+					+ "----------------------------------\n");
+		}
+		catch (NoSolutionFoundException ex) {
+			// no solution found
+			System.err.println(ex.getMessage());
+		}
+		catch (ProblemInitializationException ex) {
+			System.err.println(ex.getMessage());
+			ex.printStackTrace(System.err);
+		}
+		catch (Exception ex) {
+			System.err.println(ex.getMessage());
+			ex.printStackTrace(System.err);
+		}
+	}
+}
