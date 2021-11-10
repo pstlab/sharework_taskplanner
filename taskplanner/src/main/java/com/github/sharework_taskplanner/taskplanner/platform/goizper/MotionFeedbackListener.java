127dfb1b10788145eedeb0232f3ed282219f40df
Alessandro Umbrico <umbrico.alessandro@gmail.com>
2021-11-10 19:41:19 +0100

    updatin motion feedback goizper

--- taskplanner/src/main/java/com/github/sharework_taskplanner/taskplanner/platform/goizper/MotionFeedbackListener.java
+++ taskplanner/src/main/java/com/github/sharework_taskplanner/taskplanner/platform/goizper/MotionFeedbackListener.java
@@ -14,7 +14,7 @@ import java.util.concurrent.atomic.AtomicLong;
 /**
  *
  */
-public class MotionFeedbackListener extends RosJavaFeedbackListener<ExecuteTrajectoryActionFeedback> {
+public class MotionFeedbackListener extends RosJavaFeedbackListener<task_planner_interface_msgs.MotionResult> {
 
     private static final AtomicLong feedbackIdCounter = new AtomicLong(0);
 
@@ -34,7 +34,7 @@ public class MotionFeedbackListener extends RosJavaFeedbackListener<ExecuteTraje
      */
     @Override
     public String getMessageType() {
-        return Pose._TYPE;
+        return task_planner_interface_msgs.MotionResult._TYPE;
     }
 
     /**
@@ -44,7 +44,7 @@ public class MotionFeedbackListener extends RosJavaFeedbackListener<ExecuteTraje
      * @throws MessageUnmarshalingException
      */
     @Override
-    public PlatformFeedback unmarshal(ExecuteTrajectoryActionFeedback feedback)
+    public PlatformFeedback unmarshal(task_planner_interface_msgs.MotionResult feedback)
             throws MessageUnmarshalingException {
 
         // get dispatched command
@@ -58,14 +58,16 @@ public class MotionFeedbackListener extends RosJavaFeedbackListener<ExecuteTraje
 
         // print message
         this.log.info("[MotionFeedbackListener] Received motion feedback:\n" +
-                "- Status: " + feedback.getStatus().getStatus() + "\n" +
+                "- Status: " + feedback.getStatus() + "\n" +
                 "- Feedback: " + feedback + "\n");
 
         // prepare feedback
         PlatformFeedback pFeedback = null;
         // check feedback code
-        if (feedback != null && feedback.getStatus() != null) {
-            if (feedback.getStatus().getStatus() == 3) {
+        if (feedback != null) {
+
+            // check status code
+            if (feedback.getStatus() == 3) {
 
                 // successful execution
                 pFeedback = new PlatformFeedback(
